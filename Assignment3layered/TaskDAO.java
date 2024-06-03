822ec4e18cf373aab871512adce02590a5b17f21
Xi <568959084@qq.com>
2024-06-03 18:53:54 -0400

    Add files via upload

---
+++ Assignment3layered/TaskDAO.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class TaskDAO {
+    ArrayList<Task> taskArray = new ArrayList<>();
+
+    public TaskDAO() {
+    }
+
+    // Method to display all tasks
+    public ArrayList<Task> showTasks() {
+        if (taskArray.isEmpty()) {
+            return null;
+        } else {
+            return taskArray;
+        }
+    }
+
+    public boolean addTask(Task task) {
+        taskArray.add(task);
+        return true;
+    }
+
+    public boolean deleteTaskByName(String taskName) {
+        Task task = searchTask(taskName);
+        if (task != null) {
+            taskArray.remove(task);
+            return true;
+        }
+        return false;
+    }
+
+    public Task deleteTaskById(int id) {
+        Task task = findTaskByID(id);
+        if (task != null) {
+            taskArray.remove(task);
+            return task;
+        }
+        return null;
+    }
+
+    public boolean updateTask(int index, String newTask) {
+        if (index >= 0 && index < taskArray.size()) {
+            for (Task task : taskArray) {
+                if (task.getTaskId() == index) {
+                    task.setTaskTitle(newTask);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Task searchTask(String taskName) {
+        for (Task task : taskArray) {
+            if (task.getTaskTitle().equals(taskName)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    public Task findTaskByID(int taskID) {
+        for (Task task : taskArray) {
+            if (task.getTaskId() == taskID) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    public void assignTaskToUser(int id, String userName) {
+        Task task = findTaskByID(id);
+        if (task != null) {
+            task.setAssignedTo(userName);
+        }
+    }
+}
---
+++ Assignment3layered/TaskService.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TaskService {
+
+    private Scanner scanner = new Scanner(System.in);
+    private TaskDAO taskDAO = new TaskDAO();
+
+    public void showTasks() {
+        ArrayList<Task> taskArray = taskDAO.showTasks();
+        if (taskArray != null) {
+            for (Task task : taskArray) {
+                System.out.println(task);
+            }
+        } else {
+            System.out.println("There is no task.");
+        }
+    }
+
+    public void addTask() {
+        System.out.println("Please enter the number of tasks you would like to add: ");
+        int count = 0;
+        try {
+            count = scanner.nextInt();
+            scanner.nextLine();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+            scanner.nextLine();
+        }
+        while (count > 0) {
+            System.out.print("Enter task title: ");
+            String title = scanner.nextLine();
+            System.out.print("Enter task text: ");
+            String text = scanner.nextLine();
+            System.out.print("Enter assigned to: ");
+            String assignedTo = scanner.nextLine();
+            Task task = new Task(title, text, assignedTo);
+            boolean result = taskDAO.addTask(task);
+            if (result) {
+                System.out.println("Task added: " + task);
+            }
+            count--;
+        }
+    }
+
+    public void deleteTaskByName() {
+        System.out.print("Enter name of task to delete: ");
+        String taskName = scanner.nextLine();
+        boolean result = taskDAO.deleteTaskByName(taskName);
+        if (result) {
+            System.out.println("Task " + taskName + " was  deleted");
+        } else {
+            System.out.println("Failed to delete.");
+        }
+    }
+
+    public void deleteTaskByID() {
+        System.out.print("Enter id of task to delete: ");
+        int idToDelete = scanner.nextInt();
+        scanner.nextLine();
+        Task removedTask = taskDAO.deleteTaskById(idToDelete);
+        if (removedTask != null) {
+            System.out.println("Task removed: " + removedTask.getTaskTitle());
+        } else {
+            System.out.println("Invalid task id.");
+        }
+
+    }
+
+    public void searchTask() {
+        System.out.print("Enter task to search: ");
+        String taskToSearch = scanner.nextLine();
+        Task task = taskDAO.searchTask(taskToSearch);
+        if (task != null) {
+            System.out.println("Task found.");
+        } else {
+            System.out.println("Task not found.");
+        }
+
+    }
+
+    public void updateTaskByID() {
+        System.out.print("Enter the task id to update: ");
+        int idToUpdate = scanner.nextInt();
+        System.out.println("Enter new task name to update: ");
+        String newTask = scanner.nextLine();
+        boolean result = taskDAO.updateTask(idToUpdate, newTask);
+        if (result) {
+            System.out.println("Task updated successfully!");
+        } else {
+            System.out.println("Invalid task index.");
+        }
+    }
+
+    public void assignTaskToUser() {
+        System.out.print("Enter the task id: ");
+        int id = scanner.nextInt();
+        System.out.println("Enter user name to be assigned to: ");
+        String userName = scanner.nextLine();
+        taskDAO.assignTaskToUser(id, userName);
+    }
+
+}
